5a4e4b6219f5bbd97e291c6d854fc07fd86958b5
Jon <jon.a.mccallum@gmail.com>
2020-11-12 18:44:59 +0000

    changed port

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.get("/posts/gingerbreadoats", function(req, res){
 
 
 
-app.listen(3000, function(){
-    console.log('listening 3000');
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
