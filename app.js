3a7943832aba73cb826d38db0f4bb964dc9bfcb0
Jon <jon.a.mccallum@gmail.com>
2020-11-16 18:44:53 +0000

    about page fix

--- app.js
+++ app.js
@@ -1,40 +1,23 @@
-
-
-
 const express = require('express') 
 const bodyParser = require('body-parser')
-
-
-
-
-
 const app = express();
 
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
-
 
 
 app.get('/', function(req, res){
     res.render("index");
 });
 
-
 app.get("/about", function(req, res){
-
     res.render("about");
-
 });
 
 app.get("/blog", function(req, res){
-
-
     res.render('blog');
-
-
 });
 
 app.get("/posts/peanutsatay", function(req, res){
@@ -46,8 +29,6 @@ app.get("/posts/findinghappiness", function(req, res){
 app.get("/posts/gingerbreadoats", function(req, res){
     res.render('posts/gingerbreadoats');
 });
-
-
 
 
 const PORT = process.env.PORT || 3000;
